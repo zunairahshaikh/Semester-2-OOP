5c96b82f3837aba7fd5af4c19608bda4d617142a
Zunairah Shaikh <zunairahtdws@gmail.com>
2025-03-03 09:18:22 +0500

    Update Q4.cpp

--- Lab 5/In-Lab Tasks/Q4.cpp
+++ Lab 5/In-Lab Tasks/Q4.cpp
@@ -6,7 +6,14 @@ Create following Member Functions:
 • withdraw(double amount): Subtracts the specified amount from the account balance, if sufficient funds are available.
 • display(): Displays the account details including the account number, account holder name, and balance.
 In the main() function, create an array accounts of BankAccount objects. The array contains three elements, each representing a different bank account.
-Initialize Each BankAccount object with specific account details such as account number, holder name, and initial balance.*/
+Initialize Each BankAccount object with specific account details such as account number, holder name, and initial balance.
+Perform following operations:
+• Iterate through each account in the accounts array.
+• For each account, display the account details using the display() function.
+• Perform Two Transactions:
+• Deposits 500.0 rupees into the account.
+• Withdraws 200.0 rupees from the account.
+• After each transaction, display the updated account details, including the new balance.*/
 
 #include <iostream>
 using namespace std;
@@ -28,11 +35,8 @@ class BankAccount{
             if(balance >= amount){
                 balance -= amount;
             }
-            else{
-                cout << "Insufficient funds" << endl;
-            }
         }
-        void display() const {
+        void display() const{
             cout << "Account Number: " << accountNumber << endl;
             cout << "Account Holder Name: " << accountHolderName << endl;
             cout << "Balance: $" << balance << endl;
@@ -41,16 +45,20 @@ class BankAccount{
 
 int main(){
     BankAccount accounts[3] = {
-        BankAccount(12345, "Laiba Khan", 1000),
-        BankAccount(54321, "Aamna Rizwan", 2000),
-        BankAccount(67890, "Abeeha binte aamer", 3000)
+        BankAccount(12345, "Laiba Khan", 1000.0),
+        BankAccount(54321, "Aamna Rizwan", 2000.0),
+        BankAccount(98765, "Abeeha binte Aamer", 3000.0)
     };
 
-    cout << "Account Details: \n" << endl;
     for(int i = 0; i < 3; i++){
         accounts[i].display();
-        cout << endl;
     }
+
+    accounts[0].deposit(500.0);
+    accounts[0].display();
+
+    accounts[0].withdraw(200.0);
+    accounts[0].display();
 
     return 0;
 }
