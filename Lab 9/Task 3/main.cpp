c49c3c1f8e194494553daf59bc1858965f84ca67
Zunairah Shaikh <zunairahtdws@gmail.com>
2025-04-15 14:16:58 +0500

    Update main.cpp

--- Lab 9/Task 3/main.cpp
+++ Lab 9/Task 3/main.cpp
@@ -27,7 +27,9 @@ using namespace std;
 
 int main() {
     Book book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
-    cout << "Book Title: " << book.getTtile() << endl;
+    cout << "Book Title: " << book.getTitle() << endl;
     cout << "Book Author: " << book.getAuthor() << endl;
     cout << "Book ISBN: " << book.getISBN() << endl;
+
+    return 0;
 }
